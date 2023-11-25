91208bc799b3e2f82457c50482cb3bc36cb5984f
Guilherme OLiveira <san835117@gmail.com>
2023-11-25 20:20:02 -0300

    Salvando uma tarefa no banco de daods

--- index.js
+++ index.js
@@ -9,6 +9,33 @@ app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.post('/criar', (requisicao,reposta) => {
+    const descricao = requisicao.body.descricao
+  const completa = 0
+
+
+  const sql = `
+    INSERT INTO tarefas( descricao, completa)   
+    VALUES ('${descricao}', '${completa}')
+  `
+
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+
+})
+
 
 app.get('/', (requisicao, resposta) => {
     resposta.render('home')
