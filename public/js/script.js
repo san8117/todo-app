be77818ecaf95a0b6c608d9de18753c3141af344
Guilherme OLiveira <san835117@gmail.com>
2023-11-25 19:11:19 -0300

    deixando a mudança de tema dinâmica

---
+++ public/js/script.js
@@ -0,0 +1,47 @@
+function alterarTema () {
+    const tema = localStorage.getItem("tema")
+    const body = document.querySelector("body")
+    const button = document.querySelector(".tema-button")
+    if (tema) {
+   let novoTema
+
+        if (tema == "Light") {
+            novoTema= "dark"
+            button.innerHTML= `<img src="/imagens/sun-icon.png" alt="ícone do Sol">`
+            body.classList.remove("Light")
+            body.classList.add("dark")
+
+        } else {
+            novoTema= "Light"
+            button.innerHTML= `<img src="/imagens/moon-icon.png" alt="ícone de Lua">`
+            body.classList.remove("dark")
+            body.classList.add("Light")
+           }
+           localStorage.setItem("tema", novoTema)
+           return
+    }
+
+    localStorage.setItem("tema", "dark")
+    body.classList.add("dark")
+}
+
+
+function verificarTema () {
+     const tema = localStorage.getItem("tema")
+     const body= document.querySelector("body")
+     const button = document.querySelector(".tema-button")
+
+
+     if (tema) {
+        if(tema === "dark") {
+            body.classList.add("dark")
+            button.innerHTML= `<img src="/imagens/sun-icon.png" alt="ícone do Sol">`
+
+        } else {
+            body.classList.classList.add("Light")
+            button.innerHTML= `<img src="/imagens/moon-icon.png" alt="ícone de Lua">`
+        }
+     }
+}
+
+verificarTema()
